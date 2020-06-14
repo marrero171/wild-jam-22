f9c3d8707b05de1dac0b8a02e4ad7576b01d64d5
UA3 <rany.kamel47@gmail.com>
2020-06-14 18:21:53 -0400

    Added color-change script

---
+++ Wild Jam 22/Scenes/Vehicle/LowPolyCar.gd
@@ -0,0 +1,16 @@
+extends VehicleBody
+
+export var color = Color(0,0,0)
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Car.mesh.get("surface_1/material").set("albedo_color", color)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
