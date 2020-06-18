33f77506e4d5e3831ad341371f6842f689a3e57d
UA3 <rany.kamel47@gmail.com>
2020-06-18 17:36:37 -0400

    Set-up Car Controls

--- Wild Jam 22/Scenes/Vehicle/LeCar.gd
+++ Wild Jam 22/Scenes/Vehicle/LeCar.gd
@@ -4,11 +4,18 @@ export var color = Color(0,0,0)
 
 export var Player = false
 
+const ENGINE_FORCE = 5000.0
+const BRAKE_FORCE = 75.0
+const STEER_ANGLE = 0.5
+
 func _ready():
 	$Parts/Body/Body.mesh.get("surface_1/material").set("albedo_color", color)
 	$Parts/HeadlightFrames/HeadlightFrames.mesh.get("surface_1/material").set("albedo_color", color)
 	
 func _physics_process(_delta):
+	var forcef = 0
+	var brakef = 0
+	var steerf = 0
 	
 	if Player:
 		if Input.is_action_just_pressed("Camera"):
@@ -20,5 +27,20 @@ func _physics_process(_delta):
 			$FirstPerson.current = true
 		
 		if Input.is_action_pressed("ui_up"):
-			set_engine_force(5000.0)
-		print(engine_force)
+			forcef = 1
+		if Input.is_action_pressed("ui_down"):
+			forcef = -1
+		if Input.is_action_pressed("ui_left"):
+			steerf = 1
+		if Input.is_action_pressed("ui_right"):
+			steerf = -1
+		if Input.is_action_pressed("Brake"):
+			brakef = 1
+		
+		var rpm = round(-$Left_Front.get_rpm())
+		
+		engine_force = forcef * ENGINE_FORCE
+		brake = brakef * BRAKE_FORCE
+		steering = steerf * STEER_ANGLE
+		
+		print(rpm)
