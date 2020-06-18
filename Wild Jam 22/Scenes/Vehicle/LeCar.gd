6ec19bfc91e784a33965d6402c0b7f1a1b01625a
UA3 <rany.kamel47@gmail.com>
2020-06-18 12:52:21 -0400

    headlight frames change color, with body

--- Wild Jam 22/Scenes/Vehicle/LeCar.gd
+++ Wild Jam 22/Scenes/Vehicle/LeCar.gd
@@ -9,7 +9,8 @@ export var color = Color(0,0,0)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$StaticBody/Body/Body.mesh.get("surface_1/material").set("albedo_color", color)
-
+	$StaticBody/HeadlightFrames/HeadlightFrames.mesh.get("surface_1/material").set("albedo_color", color)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	pass
