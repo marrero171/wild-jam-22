ef668451796c8ce33c4181387d0c8011aba11a00
UA3 <rany.kamel47@gmail.com>
2020-06-18 16:31:05 -0400

    T toggles between the Cameras

--- Wild Jam 22/Scenes/Vehicle/LeCar.gd
+++ Wild Jam 22/Scenes/Vehicle/LeCar.gd
@@ -2,11 +2,19 @@ extends VehicleBody
 
 export var color = Color(0,0,0)
 
-
+export var Player = false
 
 func _ready():
 	$StaticBody/Body/Body.mesh.get("surface_1/material").set("albedo_color", color)
 	$StaticBody/HeadlightFrames/HeadlightFrames.mesh.get("surface_1/material").set("albedo_color", color)
 	
 func _physics_process(_delta):
-	pass
+	
+	if Player:
+		if Input.is_action_just_pressed("Camera"):
+			$ThirdPerson.current = not($ThirdPerson.current)
+		
+		if $ThirdPerson.current:
+			$FirstPerson.current = false
+		else:
+			$FirstPerson.current = true
