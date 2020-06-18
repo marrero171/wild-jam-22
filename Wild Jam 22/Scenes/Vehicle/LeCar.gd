64fb46843819b2a253aecdffd8075ce9d3ecde2b
UA3 <rany.kamel47@gmail.com>
2020-06-18 15:59:05 -0400

    Added Wheels to Car

--- Wild Jam 22/Scenes/Vehicle/LeCar.gd
+++ Wild Jam 22/Scenes/Vehicle/LeCar.gd
@@ -1,16 +1,12 @@
 extends VehicleBody
 
 export var color = Color(0,0,0)
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	$StaticBody/Body/Body.mesh.get("surface_1/material").set("albedo_color", color)
 	$StaticBody/HeadlightFrames/HeadlightFrames.mesh.get("surface_1/material").set("albedo_color", color)
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
+func _physics_process(_delta):
 	pass
